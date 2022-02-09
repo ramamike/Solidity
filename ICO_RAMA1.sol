e41ff8880b845c44ff9114303bc62bdc7a710cdf
ramamike <7629756@gmail.com>
2022-02-09 23:27:06 +0300

    ICO is ready and tested

--- ICO_RAMA1.sol
+++ ICO_RAMA1.sol
@@ -1,5 +1,35 @@
 pragma solidity ^0.4.18;
- 
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+} 
+
 contract Ownable {
     
     address public owner;
@@ -43,17 +73,7 @@ contract Ramamike_TokenCoin is Ownable {
     function balanceOf(address _owner) public constant returns (uint balance) {
         return balances[_owner];
     }
- 
-    function transfer(address _to, uint _value) public returns (bool success) {
-        if(balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[msg.sender] -= _value; 
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } 
-        return false;
-    }
-    
+
     function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
         if( allowed[_from][msg.sender] >= _value &&
             balances[_from] >= _value 
@@ -66,7 +86,7 @@ contract Ramamike_TokenCoin is Ownable {
         } 
         return false;
     }
-    
+
     function approve(address _spender, uint _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
@@ -84,21 +104,32 @@ contract Ramamike_TokenCoin is Ownable {
 }
 
 contract Ramamike_ICO {
-      
+
+    using SafeMath for uint;
+
     Ramamike_TokenCoin public token = new Ramamike_TokenCoin();
     
     uint start = 1644300000; // 08.02.2022 06:00:00
     
     uint period = 28;
-   
-    address public owner=msg.sender;
     
+    uint rate=1000000000000000000000; // 1 ether = 1000 toke
+
+    address public owner=msg.sender;
+     
     function() external payable {
         require(now > start && now < start + period*1 days);
-        owner.transfer(msg.value);
-        uint tokens=2222;
+        uint tokens=rate.mul(msg.value).div(1 ether);
         token.mint(msg.sender, tokens);
     }
-    
-    uint public ownerBalance=owner.balance;
+
+    function showBalance() public constant returns (uint) {
+        return this.balance;
+    }
+
+    function takeEther() public {
+        require(msg.sender==owner && now > start );
+        uint totalBalans=this.balance;
+        owner.transfer(totalBalans);
+    }
 }
